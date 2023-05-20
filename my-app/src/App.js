9abcf3819790d107b0ea25b68b82aea467f4a0a1
Artoym Shulyak <tem4ik.capper@gmail.com>
2023-05-20 17:47:58 +0300

    news changes in the project on "my-app" -> 123 Lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,47 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
+
+const Header = () => {
+  return <h2>Hello World, Artoym!</h2>;
+};
+
+// const Field = () => {
+//   const holder = "Enter here elem";
+//   const styledField = {
+//     width: "300px",
+//     background: "#ccc",
+//   };
+//   return <input placeholder={holder} type="text" style={styledField} />;
+// };
+
+class Field extends Component {
+  render() {
+    const holder = "Enter here CLASS";
+    const styledField = {
+      width: "300px",
+      background: "#ccc",
+    };
+    return <input placeholder={holder} type="text" style={styledField} />;
+  }
+}
+
+function Btn() {
+  const text = "Send";
+
+  const logged = true;
+
+  return <button>{logged ? "Enter" : text}</button>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
 
+export { Header };
 export default App;
