33c70a303dd28435ea984c74c19e773435203871
Artoym Shulyak <tem4ik.capper@gmail.com>
2023-05-20 17:57:33 +0300

    news changes in the project on my-app -> 124 Lesson

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -6,8 +6,8 @@ import { Header } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <Header />
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
