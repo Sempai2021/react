fd06a36f8de160acbf89109ecd37bd839721234f
Artoym Shulyak <tem4ik.capper@gmail.com>
2023-05-20 19:08:11 +0300

    Lesson 127 -> 1.0 version

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
-import { Component } from "react";
+// import { Component } from "react";
 import "./App.css";
 
-const Header = () => {
-  return <h2>Hello World, Artoym!</h2>;
-};
+// const Header = () => {
+//   return <h2>Hello World, Artoym!</h2>;
+// };
 
 // const Field = () => {
 //   const holder = "Enter here elem";
@@ -14,34 +14,52 @@ const Header = () => {
 //   return <input placeholder={holder} type="text" style={styledField} />;
 // };
 
-class Field extends Component {
-  render() {
-    const holder = "Enter here CLASS";
-    const styledField = {
-      width: "300px",
-      background: "#ccc",
-    };
-    return <input placeholder={holder} type="text" style={styledField} />;
-  }
-}
+// class Field extends Component {
+//   render() {
+//     const holder = "Enter here CLASS";
+//     const styledField = {
+//       width: "300px",
+//       background: "#ccc",
+//     };
+//     return <input placeholder={holder} type="text" style={styledField} />;
+//   }
+// }
 
-function Btn() {
-  const text = "Send";
+// function Btn() {
+//   const text = "Send";
 
-  const logged = true;
+//   const logged = true;
 
-  return <button>{logged ? "Enter" : text}</button>;
+//   return <button>{logged ? "Enter" : text}</button>;
+// }
+
+function WhoAmi({ name, surname }) {
+  return (
+    <div>
+      <h1>
+        Ma name is {name()}, surname - {surname}
+      </h1>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Field />
-      <Btn />
+      <WhoAmi
+        name={() => {
+          return "Artoym";
+        }}
+        surname="Shulyak"
+      />
+      <WhoAmi
+        name={() => {
+          return "Dima";
+        }}
+        surname="Shulyak"
+      />
     </div>
   );
 }
 
-export { Header };
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,12 +2,10 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Header } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <Header />
     <App />
   </StrictMode>
 );
